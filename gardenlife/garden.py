f080b38be21a15a909b9956fb2e5e28933bfcb9d
jonboland <jon.boland@gmail.com>
2020-05-22 09:58:06 +0100

    Add garden

---
+++ gardenlife/garden.py
@@ -0,0 +1,60 @@
+from datetime import datetime, date
+from time import strftime
+
+
+class Garden:
+    """Class to represent a garden."""
+
+    def __init__(self, name, location, size, since, *owners):
+        self.name = name
+        self.location = location
+        self.size = size
+        self.owners = owners
+        self.since = since
+        self.creatures = []
+        self.plants = []
+        self.tasks = []
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__} in {self.location} called {self.name}, "
+            f"which is approximately {self.size} acres in size."
+        )
+
+    def ownership(self):
+        """Return who currently owns the garden."""
+        if len(self.owners) == 1:
+            return f"The owner of {self.name} is {self.owners[0]}."
+        elif len(self.owners) == 2:
+            return f"The owners of {self.name} are {' and '.join(self.owners)}."
+        return (
+            f"The owners of {self.name} are {', '.join(self.owners[:-1])} "
+            f"and {self.owners[-1]}."
+        )
+
+    def ownership_length(self):
+        """Return current garden ownership length in days."""
+        now = datetime.strptime(str(date.today()), "%Y-%m-%d")
+        ago = datetime.strptime(self.since, "%d/%m/%Y")
+        dif = now - ago
+        return f"{self.name} has been in the same hands for {dif.days} days."
+
+    @classmethod
+    def from_string(cls, garden_details):
+        """Construct a garden from a hyphen separated string."""
+        name, location, size, since, *owners = garden_details.split("-")
+        return cls(name, location, size, since, *owners)
+
+    @staticmethod
+    def season():
+        """Return the current season."""
+        current_month = strftime("%B")
+        seasons = {
+            "Spring": ["March", "April", "May"],
+            "Summer": ["June", "July", "August"],
+            "Autumn": ["September", "October", "November"],
+            "Winter": ["December", "January", "February"],
+        }
+        for s, m in seasons.items():
+            if current_month in m:
+                return s
