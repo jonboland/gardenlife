14e29a8d067bc4d8fe2a27de8ae52208fccdf0ac
jonboland <jon.boland@gmail.com>
2020-07-07 21:14:14 +0100

    Add creature tests

---
+++ tests/test_creature.py
@@ -0,0 +1,57 @@
+import pytest
+
+import context
+from creature import Creature
+
+
+@pytest.fixture
+def test_creature():
+    return Creature(
+        "mammal",
+        "badger",
+        3,
+        "03/07/20",
+        impact="positive",
+        prevalence="low",
+        trend="stable",
+    )
+
+
+def test_trend_increasing(test_creature):
+    test_creature.change_level("trend", "increase")
+    assert test_creature.trend == "increasing"
+
+
+def test_trend_already_rapidly_increasing(test_creature, capsys):
+    for x in range(3):
+        test_creature.change_level("trend", "increase")
+    captured = capsys.readouterr()
+    assert captured.out == "trend already set to rapidly increasing\n"
+
+
+def test_prevalence_very_low(test_creature):
+    test_creature.change_level("prevalence", "decrease")
+    assert test_creature.prevalence == "very low"
+
+
+def test_impact_already_very_negative(test_creature, capsys):
+    for x in range(4):
+        test_creature.change_level("impact", "decrease")
+    captured = capsys.readouterr()
+    assert captured.out == "impact already set to very negative\n"
+
+
+def test_invalid_impact_level(test_creature):
+    with pytest.raises(ValueError) as excinfo:
+        test_creature.impact = "high"
+    assert "high is not a valid impact level" in str(excinfo.value)
+
+
+def test_invalid_direction(test_creature):
+    with pytest.raises(ValueError) as excinfo:
+        test_creature.change_level("prevalence", "up")
+    assert "up is not a valid direction" in str(excinfo.value)
+
+
+if __name__ == "__main__":
+    pytest.main()
