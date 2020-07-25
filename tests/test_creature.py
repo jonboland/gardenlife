d7c5fc975803f4ab41ad6f9ad93cab301c466aa7
jonboland <jon.boland@gmail.com>
2020-07-25 10:30:54 +0100

    Changed fixture names

--- tests/test_creature.py
+++ tests/test_creature.py
@@ -5,48 +5,48 @@ from creature import Creature
 
 
 @pytest.fixture
-def test_creature():
+def badger():
     return Creature("mammal", "badger", "03/07/2020", 3, "positive", "low", "stable")
 
 
-def test_trend_increasing(test_creature):
-    test_creature.change_level("trend", "increase")
-    assert test_creature.trend == "increasing"
+def test_trend_increasing(badger):
+    badger.change_level("trend", "increase")
+    assert badger.trend == "increasing"
 
 
-def test_trend_already_rapidly_increasing(test_creature, capsys):
+def test_trend_already_rapidly_increasing(badger, capsys):
     for x in range(3):
-        test_creature.change_level("trend", "increase")
+        badger.change_level("trend", "increase")
     captured = capsys.readouterr()
     assert captured.out == "trend already set to rapidly increasing\n"
 
 
-def test_prevalence_very_low(test_creature):
-    test_creature.change_level("prevalence", "decrease")
-    assert test_creature.prevalence == "very low"
+def test_prevalence_very_low(badger):
+    badger.change_level("prevalence", "decrease")
+    assert badger.prevalence == "very low"
 
 
-def test_impact_already_very_negative(test_creature, capsys):
+def test_impact_already_very_negative(badger, capsys):
     for x in range(4):
-        test_creature.change_level("impact", "decrease")
+        badger.change_level("impact", "decrease")
     captured = capsys.readouterr()
     assert captured.out == "impact already set to very negative\n"
 
 
-def test_invalid_impact_level(test_creature):
+def test_invalid_impact_level(badger):
     with pytest.raises(ValueError) as excinfo:
-        test_creature.impact = "high"
+        badger.impact = "high"
     assert "high is not a valid impact level" in str(excinfo.value)
 
 
-def test_invalid_direction(test_creature):
+def test_invalid_direction(badger):
     with pytest.raises(ValueError) as excinfo:
-        test_creature.change_level("prevalence", "blank")
+        badger.change_level("prevalence", "blank")
     assert "blank is not a valid direction" in str(excinfo.value)
 
 
-def test_date_appeared(test_creature):
-    assert test_creature.appeared == "03/07/2020"
+def test_date_appeared(badger):
+    assert badger.appeared == "03/07/2020"
 
 
 if __name__ == "__main__":
