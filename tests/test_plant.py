bf5fcf8787e9d32dff13728f84f064b1cdf8812b
jonboland <jon.boland@gmail.com>
2020-09-07 08:13:06 +0100

    Increased coverage

--- tests/test_plant.py
+++ tests/test_plant.py
@@ -20,6 +20,29 @@ def test_trend_already_rapidly_decreasing(sunflower, capsys):
     captured = capsys.readouterr()
     assert captured.out == "trend already set to rapidly decreasing\n"
 
+def test_prevalence_high(sunflower):
+    sunflower.change_level("prevalence", "increase")
+    assert sunflower.prevalence == "high"
+
+
+def test_impact_already_very_positive(sunflower, capsys):
+    for x in range(3):
+        sunflower.change_level("impact", "increase")
+    captured = capsys.readouterr()
+    assert captured.out == "impact already set to very positive\n"
+
+
+def test_invalid_trend_level(sunflower):
+    with pytest.raises(ValueError) as excinfo:
+        sunflower.trend = "neutral"
+    assert "neutral is not a valid trend level" in str(excinfo.value)
+
+
+def test_invalid_direction(sunflower):
+    with pytest.raises(ValueError) as excinfo:
+        sunflower.change_level("impact", "blank")
+    assert "blank is not a valid direction" in str(excinfo.value)
+
 
 def test_date_appeared(sunflower):
     assert sunflower.planted == "01/05/2020"
