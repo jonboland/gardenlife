eca91c113e06519546e8068111bb2c4a58ee677f
jonboland <jon.boland@gmail.com>
2020-10-14 11:35:49 +0100

    Revised context

--- tests/context.py
+++ tests/context.py
@@ -2,5 +2,5 @@ from pathlib import Path
 import sys
 
 # Enables gardenlife package imports when running tests
-context = Path.cwd().parent / "gardenlife"
+context = Path(__file__).resolve().parents[1] / "gardenlife"
 sys.path.insert(0, str(context))
