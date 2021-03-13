06b3718763a812f9709b17794487d6d01d54143a
jonboland <jon.boland@gmail.com>
2021-03-13 08:23:20 +0000

    Added manage creatures functionality

--- gardenlife/manage.py
+++ gardenlife/manage.py
@@ -1,6 +1,7 @@
 from calendar import Calendar
 import pickle
 from tkinter.constants import SUNKEN
+import traceback
 
 import PySimpleGUI as sg
 
@@ -10,6 +11,10 @@ from task import Task
 
 
 sg.theme(new_theme="LightGray1")
+sg.theme_button_color(("white", "#004225"))
+sg.theme_input_background_color("light grey")
+sg.theme_input_text_color("black")
+sg.theme_slider_color("#004225")
 
 
 try:
@@ -35,7 +40,7 @@ summary_details = {
     "Current season:": garden.season(),
     "Garden name:": garden.name,
     "Location:": garden.location,
-    "Size:": f"{garden.size} acres",
+    "Size:": garden.garden_size(),
     "Owned by:": garden.ownership(),
     "Owned for:": garden.ownership_length(),
     "Total creatures:": len(garden.creatures),
@@ -44,39 +49,25 @@ summary_details = {
     "Outstanding tasks:": 0,
 }
 
-
+# fmt: off
 summary = [
     [
         sg.Text(label, size=(15, 1), pad=(0, 10), justification="right"),
-        sg.Text(
-            value,
-            size=(30, 1),
-            relief=SUNKEN,
-            # enable_events=True,
-            key=f"-SUMMARY {label[:-1].upper()}-",
-        ),
+        sg.Text(value, size=(30, 1), relief=SUNKEN, key=f"-SUMMARY {label[:-1].upper()}-"),
     ]
     for label, value in summary_details.items()
 ]
-
+# fmt: on
 
 report_buttons = [
-    [
-        sg.Button(
-            name,
-            size=(20, 2),
-            pad=(0, 10),
-            border_width=2,
-            button_color="white on #004225",
-        )
-    ]
+    [sg.Button(name, size=(20, 2), pad=(0, 10), border_width=2)]
     for name in REPORT_BUTTON_TEXT
 ]
 
 
 summary_tab = [
     [
-        sg.Text("", size=(1, 20)),  # Blank row to set column sizes
+        # sg.Text("", size=(1, 20)),  # Blank row to set column sizes
         sg.Column(summary, pad=((30, 40), 40)),
         sg.Column(report_buttons, size=(200, 190), pad=((25, 40), 0)),
     ]
@@ -102,16 +93,15 @@ def garden_label_format(label):
     return sg.Text(label, size=(15, 1), pad=(0, 10), justification="right")
 
 
-garden_elements = [
-    [
+select_garden = [
         garden_label_format("Select garden:"),
         sg.Combo([], default_value=garden.name, size=(30, 1), key="-SELECT GARDEN-"),
-    ]
 ]
 
-garden_elements += [[sg.Text("", size=(0, 1))]]  # Blank row to add space below selector
 
-garden_elements += [
+garden_blank = [sg.Text("", size=(0, 1))]  # Blank row to add space below selector
+
+garden_details = [
     [
         garden_label_format(label),
         sg.Input(value, size=(30, 1), key=f"-{label[:-1].upper()}-"),
@@ -119,18 +109,11 @@ garden_elements += [
     for label, value in garden_details.items()
 ]
 
-garden_elements += [
-    [
-        sg.Button(
-            name,
-            size=(15, 2),
-            pad=((43, 0), 30),
-            button_color="white on #004225",
-        )
-        for name in GARDEN_BUTTON_TEXT
-    ]
+garden_buttons = [
+    sg.Button(name, size=(15, 2), pad=((43, 0), 30)) for name in GARDEN_BUTTON_TEXT
 ]
 
+garden_elements = [select_garden, garden_blank] + garden_details + [garden_buttons]
 
 garden_tab = [[sg.Column(garden_elements, pad=((30, 40), 40))]]
 
@@ -138,80 +121,126 @@ garden_tab = [[sg.Column(garden_elements, pad=((30, 40), 40))]]
 # ------------------------------ Manage Creatures Tab ------------------------------ #
 
 
-CREATURE_BUTTON_TEXT = ("CREATE CREATURE", "UPDATE CREATURE")
+CREATURE_BUTTON_TEXT = ("CREATE/UPDATE", "REMOVE")
+CREATURE_FIELD_SIZE = (25, 1)
 
 
-creature_details = {
-    "Creature name:": 0,
-    "Creature type:": 0,
-    "Appeared date:": 0,
-    "Age:": 0,
-    "Status:": 0,
-}
+def creature_label(label):
+    return sg.Text(label, size=(13, 1), pad=(0, 10))
 
 
-def creature_label_format(label):
-    return sg.Text(label, size=(15, 1), pad=(0, 10), justification="right")
+def creature_slider_label(label):
+    return sg.Text(label, size=(12, 1), pad=((0, 8), (20, 0)))
 
 
-creature_elements = [
-    [
-        creature_label_format("Creature name:"),
-        sg.Combo(
-            [creature for creature in garden.creatures],
-            default_value=0,
-            size=(30, 1),
-            key="-CREATURE NAME-",
-        ),
-    ]
+def creature_slider(key=None, tooltip=None):
+    return sg.Slider(
+        range=(1, 5),
+        key=key,
+        orientation="horizontal",
+        default_value=3,
+        size=(19.7, 19),
+        tooltip=tooltip,
+    )
+
+
+creature_name = [
+    creature_label("Creature name:"),
+    sg.Combo(
+        sorted([""] + [creature for creature in garden.creatures]),
+        size=CREATURE_FIELD_SIZE,
+        key="-CREATURE NAME-",
+        enable_events=True,
+    ),
+]
+# fmt: off
+creature_type = [
+    creature_label("Creature type:"),
+    sg.Combo(
+        sorted([""] + [creature.creature_type for creature in garden.creatures.values()]),
+        size=CREATURE_FIELD_SIZE,
+        key="-CREATURE TYPE-",
+    ),
+]
+
+creature_age = [
+    creature_label("Creature age:"),
+    sg.Input(size=CREATURE_FIELD_SIZE, key="-CREATURE AGE-"),
+]
+
+creature_appeared = [
+    sg.Text("Appeared date:", size=(13, 1), pad=(0, (6, 30))),
+    sg.Input(size=CREATURE_FIELD_SIZE, key="-CREATURE APPEARED DATE-", pad=(5, (6, 30))),
+    sg.CalendarButton("PICK", format="%d/%m/%Y", pad=(0, (6, 30))),
+]
+
+creature_impact = [
+    creature_slider_label("Impact level:"),
+    creature_slider(
+        key="-CREATURE IMPACT SLIDER-",
+        tooltip="Impact levels — 1: very negative, 2: negative, "
+                "3: neutral, 4: positive, 5: very positive",
+    ),
+]
+
+creature_prevalence = [
+    creature_slider_label("Prevalence level:"),
+    creature_slider(
+        key="-CREATURE PREVALENCE SLIDER-", 
+        tooltip="Prevalence levels — 1: very low, 2: low, "
+                "3: medium, 4: high, 5: very high",
+    ),
+]
+
+creature_trend = [
+    creature_slider_label("Trend level:"),
+    creature_slider(
+        key="-CREATURE TREND SLIDER-", 
+        tooltip="Trend levels — 1: rapidly decreasing, 2: decreasing, "
+                "3: stable, 4: increasing, 5: rapidly increasing",
+    ),
+]
+
+creature_status = [
+    sg.Text("Status:", size=(8, 1), pad=(0, 10)),
+    sg.Combo(["current", "archived"], size=CREATURE_FIELD_SIZE, key="-CREATURE STATUS-"),
+]
+# fmt: on
+creature_notes_label = [sg.Text("Notes:", size=(8, 1), pad=(0, 10))]
+
+creature_notes_field = [
+    sg.Multiline(size=(35, 10), pad=(0, 10), key="-CREATURE NOTES-", do_not_clear=False)
+]
+
+creature_buttons = [
+    sg.Button(name, size=(15, 2), pad=((0, 7), (35, 0)), key=f"CREATURE {name}")
+    for name in CREATURE_BUTTON_TEXT
 ]
 
 
-creature_elements += [
-    [
-        creature_label_format("Creature type:"),
-        sg.Combo(
-            ["one", "two", "three"],
-            default_value=0,
-            size=(30, 1),
-            key="-CREATURE TYPE-",
-        ),
-    ]
+creatures_left_column = [
+    creature_name,
+    creature_type,
+    creature_age,
+    creature_appeared,
+    creature_impact,
+    creature_prevalence,
+    creature_trend,
 ]
 
-
-creature_elements += [
-    [
-        creature_label_format("Appeared date:"),
-        sg.Input(size=(30, 1), key="-APPEARED DATE-"),
-        sg.CalendarButton("PICK", button_color="white on #004225", format="%d/%m/%Y"),
-    ]
+creatures_right_column = [
+    creature_status,
+    creature_notes_label,
+    creature_notes_field,
+    creature_buttons,
 ]
 
-
-creature_elements += [
+creatures_tab = [
     [
-        creature_label_format("Age:"),
-        sg.Input(size=(30, 1), key="-CREATURE AGE-"),
+        sg.Column(creatures_left_column, pad=((30, 40), 40)),
+        sg.Column(creatures_right_column, pad=((0, 40), 40), vertical_alignment="top"),
     ]
 ]
-
-
-creature_elements += [
-    [
-        sg.Button(
-            name,
-            size=(18, 2),
-            pad=((43, 0), 30),
-            button_color="white on #004225",
-            # key=f"-CREATE {name}-",
-        )
-        for name in CREATURE_BUTTON_TEXT
-    ]
-]
-
-
-creatures_tab = [[sg.Column(creature_elements, pad=((30, 40), 40))]]
 
 
 # -------------------------------- Manage Plants Tab ------------------------------- #
@@ -247,9 +276,9 @@ tab_details = {
 
 all_tabs = [[sg.Tab(label, tab, pad=(10, 10)) for label, tab in tab_details.items()]]
 
-
-layout = [[sg.Menu(menu_definition)], [sg.TabGroup(all_tabs)]]
-
+# fmt: off
+layout = [[sg.Menu(menu_definition)], [sg.TabGroup(all_tabs, tab_background_color="#004225")]]
+# fmt: on
 # Create the window
 window = sg.Window("gardenlife", layout, keep_on_top=True)
 
@@ -257,7 +286,7 @@ window = sg.Window("gardenlife", layout, keep_on_top=True)
 # ----------------------------------- Event Loop ----------------------------------- #
 
 
-# Display and interact with the Window using an Event Loop
+# Display and interact with the window using an event loop
 while True:
     event, values = window.read()
     print(event, values)
@@ -288,20 +317,80 @@ while True:
         # Then update the associated summary fields
         window["-SUMMARY GARDEN NAME-"].update(garden.name)
         window["-SUMMARY LOCATION-"].update(garden.location)
-        window["-SUMMARY SIZE-"].update(f"{garden.size} acres")
+        window["-SUMMARY SIZE-"].update(garden.garden_size())
         window["-SUMMARY OWNED BY-"].update(garden.ownership())
         window["-SUMMARY OWNED FOR-"].update(garden.ownership_length())
 
     ####################### Manage Creatures Events ########################
 
-    elif event == "CREATE CREATURE":
-        pass
+    def clear_creature_values():
+        for value in ("NAME", "TYPE", "AGE", "APPEARED DATE", "STATUS", "NOTES"):
+            window[f"-CREATURE {value}-"].update("")
+        for value in ("IMPACT", "PREVALENCE", "TREND"):
+            window[f"-CREATURE {value} SLIDER-"].update(3)
 
+
+    def creature_instance():
+        return garden.creatures[values["-CREATURE NAME-"]]
+
+
+    def update_creature_dropdowns():
+        creature_names = sorted([""] + [creature for creature in garden.creatures])
+        creature_types = sorted(
+            [""] + [creature.creature_type for creature in garden.creatures.values()]
+        )
+        return (
+            window["-CREATURE NAME-"].update(values=creature_names),
+            window["-CREATURE TYPE-"].update(values=creature_types),
+        )
+
+    # fmt: off
+    try:
+        if event == "CREATURE CREATE/UPDATE":
+            creature = Creature(
+                creature_name=values["-CREATURE NAME-"],
+                creature_type=values["-CREATURE TYPE-"],
+                age=values["-CREATURE AGE-"],
+                appeared=values["-CREATURE APPEARED DATE-"],
+                notes=values["-CREATURE NOTES-"],
+                impact=values["-CREATURE IMPACT SLIDER-"],
+                prevalence=values["-CREATURE PREVALENCE SLIDER-"],
+                trend=values["-CREATURE TREND SLIDER-"],
+            )
+            if values["-CREATURE STATUS-"] == "archived":
+                creature.status.archive()
+            garden.add_item("creatures", creature)
+            update_creature_dropdowns()
+            clear_creature_values()
+            window["-SUMMARY TOTAL CREATURES-"].update(len(garden.creatures))
+
+        elif event == "CREATURE REMOVE":
+            garden.remove_item("creatures", values["-CREATURE NAME-"])
+            update_creature_dropdowns()
+            clear_creature_values()
+            window["-SUMMARY TOTAL CREATURES-"].update(len(garden.creatures))
+
+        elif values["-CREATURE NAME-"] == "":
+            clear_creature_values()
+
+        # If a creature is selected populate the relevant fields with its values
+        elif values["-CREATURE NAME-"]:  # Something is highlighted in the dropdown
+            window["-CREATURE TYPE-"].update(creature_instance().creature_type)
+            window["-CREATURE AGE-"].update(creature_instance().age)
+            window["-CREATURE APPEARED DATE-"].update(creature_instance().appeared)
+            window["-CREATURE STATUS-"].update(creature_instance().status.get())
+            window["-CREATURE NOTES-"].update(creature_instance().notes)
+            window["-CREATURE IMPACT SLIDER-"].update(creature_instance().impact)
+            window["-CREATURE PREVALENCE SLIDER-"].update(creature_instance().prevalence)
+            window["-CREATURE TREND SLIDER-"].update(creature_instance().trend)
+
+    except ValueError as error_description:
+        sg.popup(error_description, keep_on_top=True)
+    # fmt: on
 
     ######################### Manage Plant Events ##########################
 
     ########################## Manage Task Events ##########################
 
 # Finish up by removing from the screen
-
 window.close()
