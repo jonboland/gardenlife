a4214b95760792708fe10b94da97abe270a7f842
jonboland <jon.boland@gmail.com>
2021-04-19 13:40:29 +0100

    Updated garden tests following changes to ownership and ownership length methods

--- tests/test_garden.py
+++ tests/test_garden.py
@@ -2,6 +2,8 @@ import pytest
 
 import context
 import garden
+import organisms
+import task
 
 
 @pytest.fixture
@@ -9,15 +11,29 @@ def shade():
     return garden.Garden("Shade", "Hull", 1, "04/02/1987", "Dave Davidson")
 
 
+@pytest.fixture
+def badger():
+    return organisms.Creature(
+        "badger",
+        "mammal",
+        "03/07/2020",
+        "Digs holes in various parts of the garden.",
+        10,
+        3,
+        2,
+        4,
+    )
+
+
 def test_one_owner(shade):
-    assert shade.ownership() == "The owner of Shade is Dave Davidson."
+    assert shade.ownership() == "Dave Davidson"
 
 
 def test_two_owners():
     light = garden.Garden(
         "Light", "London", 0.2, "13/11/2017", "Paul Daniels", "Ruby Wilson"
     )
-    assert light.ownership() == "The owners of Light are Paul Daniels and Ruby Wilson."
+    assert light.ownership() == "Paul Daniels and Ruby Wilson"
 
 
 def test_three_owners_from_string():
@@ -26,42 +42,42 @@ def test_three_owners_from_string():
     )
     assert (
         string_garden.ownership()
-        == "The owners of Bell are Mary Jones, Greg Jones and Spot."
+        == "Mary Jones, Greg Jones and Spot"
     )
 
 
 def test_ownership_lenght_less_than_two_years(shade):
     assert (
         shade.ownership_length("25/01/1989")
-        == "Shade has been in the same hands for 721 days."
+        == "721 days"
     )
 
 
 def test_ownership_lenght_over_17_years(shade):
     assert (
         shade.ownership_length("22/03/2004")
-        == "Shade has been in the same hands for 6,256 days.\nThat's around 17 years."
+        == "6,256 days (around 17 years)"
     )
 
 
 def test_ownership_lenght_around_33_and_a_half_year(shade):
     assert (
         shade.ownership_length("22/07/2020")
-        == "Shade has been in the same hands for 12,222 days.\nThat's around 33.5 years."
+        == "12,222 days (around 33.5 years)"
     )
 
 
 def test_ownership_lenght_nearly_40_years(shade):
     assert (
         shade.ownership_length("01/01/2027")
-        == "Shade has been in the same hands for 14,576 days.\nThat's around 40 years."
+        == "14,576 days (around 40 years)"
     )
 
 
 def test_ownership_lenght_one_day(shade):
     assert (
         shade.ownership_length("05/02/1987")
-        == "Shade has been in the same hands for 1 day."
+        == "1 day"
     )
 
 
@@ -71,7 +87,7 @@ def test_ownership_lenght_zero_days():
     )
     assert (
         new_shade.ownership_length()
-        == "New Shade has been in the same hands for 0 days."
+        == "0 days"
     )
 
 
@@ -83,6 +99,32 @@ def test_season_current(shade):
 def test_season_specified(shade):
     seasons = ("Spring", "Summer", "Autumn", "Winter")
     assert shade.season("December") == "Winter"
+
+
+def test_add_item_valueerror(shade, badger):
+    with pytest.raises(ValueError) as excinfo:
+        shade.add_item("animals", badger)
+    assert str(excinfo.value) == "animals is not a valid category"
+
+
+def test_add_item_creature(shade, badger):
+    shade.add_item("creatures", badger)
+    assert shade.creatures["badger"] == badger
+
+
+def test_add_item_plant(shade):
+    carrot = organisms.Plant("carrot", "vegetable", notes="In upper veg patch.")
+    shade.add_item("plants", carrot)
+    assert shade.plants["carrot"] == carrot
+
+
+def test_add_item_task(shade):
+    cut_hedges = task.Task("cut hedges", "01/05/2020")
+    shade.add_item("tasks", cut_hedges)
+    assert shade.tasks["cut hedges"] == cut_hedges
+
+def test_str_representation(shade):
+    assert str(shade) == "Garden in Hull called Shade, which is approximately 1 acre in size."
 
 
 if __name__ == "__main__":
