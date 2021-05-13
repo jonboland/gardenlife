3df17029fe44f76a67746348dc59dd8c1e39591e
jonboland <jon.boland@gmail.com>
2021-05-13 15:37:51 +0100

    Major refactoring of organisms to reflect changes to parent class

--- gardenlife/organisms.py
+++ gardenlife/organisms.py
@@ -2,83 +2,19 @@ from organism import Organism
 
 
 class Creature(Organism):
-    def __init__(
-        self,
-        creature_name,
-        creature_type,
-        appeared=None,
-        notes=None,
-        age=None,
-        impact=3,
-        prevalence=3,
-        trend=3,
-    ):
-        super().__init__(
-            notes,
-            age,
-            impact,
-            prevalence,
-            trend,
-            organism_kind="creature",
-        )
-        self.creature_name = creature_name  # Eg Badger, Mouse, Bee, Pigeon
-        self.creature_type = creature_type  # Eg Mammal, Rodent, Insect, Bird
+    def __init__(self, *args, appeared=None, **kwargs):
+        super().__init__(*args, **kwargs)
         self.appeared = appeared
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.creature_name}, {self.creature_type})"
-
     def __str__(self):
-        if self.creature_name[0] in "aeiou":
-            name = f"This creature is an {self.creature_name}"
-        else:
-            name = f"This creature is a {self.creature_name}"
-        return f"{name}, which is a {self.creature_type}."
-
-    def __eq__(self, other):
-        return (
-            self.creature_type == other.creature_type
-            and self.creature_name == other.creature_name
-        )
+        return f"Creature name: {self.name}. Creature type: {self.org_type}."
 
 
 class Plant(Organism):
-    def __init__(
-        self,
-        plant_name,
-        plant_type,
-        edible=False,
-        planted=None,
-        notes=None,
-        age=None,
-        impact=3,
-        prevalence=3,
-        trend=3,
-    ):
-        super().__init__(
-            notes,
-            age,
-            impact,
-            prevalence,
-            trend,
-            organism_kind="plant",
-        )
-        self.plant_name = plant_name  # Eg Ash, Bean, Poppy, Thistle
-        self.plant_type = plant_type  # Eg Tree, Vegetable, Flower, Weed
+    def __init__(self, *args, edible=False, planted=None, **kwargs):
+        super().__init__(*args, **kwargs)
         self.edible = edible
         self.planted = planted
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.plant_name}, {self.plant_type})"
-
     def __str__(self):
-        if self.plant_name[0] in "aeiou":
-            name = f"This plant is an {self.plant_name}"
-        else:
-            name = f"This plant is a {self.plant_name}"
-        return f"{name}, which is a {self.plant_type}."
-
-    def __eq__(self, other):
-        return (
-            self.plant_type == other.plant_type and self.plant_name == other.plant_name
-        )
+        return f"Plant name: {self.name}. Plant type: {self.org_type}."
